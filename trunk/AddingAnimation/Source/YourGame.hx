c5deb7b5ebd6be25de76babd44fa218a64a1f9ae
langquangButin@gmail.com <langquangButin@gmail.com@126ff72f-7a9e-b8bf-ea38-fd371757954f>
2013-09-17 18:03:56 +0000

    zoom + align image

--- trunk/AddingAnimation/Source/YourGame.hx
+++ trunk/AddingAnimation/Source/YourGame.hx
@@ -1,11 +1,17 @@
 package ;
+import flash.display.Bitmap;
+import flash.display.BitmapData;
 import flash.display.Sprite;
+import flash.text.TextField;
 import openfl.display.FPS;
 import qtframework.display.Image;
 import qtframework.qtcore.Game;
 import qtframework.textures.Texture;
 import qtframework.display.MovieClips;
 import qtframework.qtcore.Starling;
+import flash.events.KeyboardEvent;
+import flash.ui.Keyboard;
+import flash.events.Event;
 
 /**
  * ...
@@ -14,6 +20,8 @@ import qtframework.qtcore.Starling;
 class YourGame extends  Game
 {
 
+	private var mTxt : TextField ;
+	private var mNum : TextField ;
 	public function new(game_width : Float, game_height : Float) 
 	{
 		super(game_width, game_height);
@@ -26,25 +34,89 @@ class YourGame extends  Game
 	override public function gameInit():Void
 	{
 	
-		var image : Image = new Image(Starling.sCurrent.resources.getFrame("bg", "bg0000"));
-		image.x = 0;
-		image.y = 0;
-		//image.setGlowFilter(0x00ff00);
-		addChild(image);
+		var bg : Image = new Image(Starling.sCurrent.resources.getFrame("bg", "bg0000"));
+		bg.x = 0;
+		bg.y = 0;
+		addChild(bg);
 		
-		for(i in 0...500)
-		{
-			var frames : Array<Texture> = Starling.sCurrent.resources.getSequenceFrame("rada","Rada");
-			var mc : MovieClips = new MovieClips(frames,30);
-			mc.x =  Math.random()*gameWidth;
-			mc.y = Math.random()*gameHeight;
-			//mc.setGlowFilter(0xff0000);
-			addChild(mc);
-			Starling.sCurrent.juggler.add(mc);
-		}
+		var bitmapData : BitmapData = new BitmapData(100, 100, false,0xFF0000);
+		var image : Image = new Image(new Texture(bitmapData), 0);
+		image.x = 200;
+		image.y = 200;
+		image.alpha = 0.5;
+		addChild(image);
 		
 		
 		addChild( new FPS() );
+		mTxt = new TextField();
+		mTxt.x = 200;
+		addChild(mTxt);
+		
+		mNum = new TextField();
+		mNum.x = 400;
+		addChild(mNum);
+		
+		
+		stage.addEventListener(KeyboardEvent.KEY_DOWN, onKeyDown);
+		stage.addEventListener (Event.ENTER_FRAME, this_onEnterFrame);
+		stage.addEventListener (KeyboardEvent.KEY_UP, stage_onKeyUp);
+	}
+	
+	public var zoomIn : Bool;
+	public var zoomOut : Bool;
+	public function onKeyDown(e : KeyboardEvent):Void
+	{
+		if ( e.keyCode == Keyboard.UP )
+		{
+			zoomOut = true;
+		}
+		else if(e.keyCode == Keyboard.DOWN){
+			zoomIn = true;
+		}
+	}
+	
+	private function this_onEnterFrame (event:Event):Void 
+	{
+		if ( zoomIn )
+		{
+			scaleX  += 0.01;
+			scaleY  += 0.01;
+		}
+		else if(zoomOut){
+			scaleX  -= 0.01;
+			scaleY  -= 0.01;
+		}
+		
+		mTxt.text = Std.string(scaleX);
+	}
+	
+	private var mN : Int = 0;
+	private function stage_onKeyUp (event:KeyboardEvent):Void {
+		zoomOut = false;
+		zoomIn = false;
+		
+		if ( event.keyCode == Keyboard.SPACE )
+		{
+					for(i in 0...500)
+					{
+						var frames : Array<Texture> = Starling.sCurrent.resources.getSequenceFrame("rada","Rada");
+						var mc : MovieClips = new MovieClips(frames,30);
+						mc.x =  Math.random()*gameWidth;
+						mc.y = Math.random()*gameHeight + 20;
+						addChild(mc);
+						Starling.sCurrent.juggler.add(mc);
+						
+						//var mc : Image = new Image(Starling.sCurrent.resources.getFrame("rada", "Rada0001"));
+						//mc.x =  Math.random()*gameWidth;
+						//mc.y = Math.random()*gameHeight+ 20;
+						//addChild(mc);
+
+					
+					
+					}
+					mN += 500;
+					mNum.text = Std.string(mN);
+		}
 	}
 	
 }
---
+++ trunk/AddingAnimation/Source/qtframework/defines/DisplayAlign.hx
@@ -0,0 +1,16 @@
+package qtframework.defines;
+
+/**
+ * ...
+ * @author butin
+ */
+class DisplayAlign
+{
+	inline public static var DEFAULT:Int = 0;
+	inline public static var CENTER:Int = 1;
+	public function new() 
+	{
+		
+	}
+	
+}
